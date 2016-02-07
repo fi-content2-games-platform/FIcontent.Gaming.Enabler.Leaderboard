8a8df436fe6dd9c291494d8a672ce7dc85b6fbcb
lephleg <lephleg@gmail.com>
2016-02-07 07:14:37 +0200

    Add up scores functionality implemented.

---
+++ leaderboard-standalone/src/ScoreEntry.java
@@ -0,0 +1,31 @@
+import java.lang.String;
+
+public class ScoreEntry {
+	
+	private String name;
+	private int value;
+	
+	//constructor
+	public ScoreEntry(String a, int b) {
+		setScoreName(a);
+	    setScoreValue(b);
+	}
+
+	public int getScoreValue() {
+		return value;
+	}
+
+	public void setScoreValue(int value) {
+		this.value = value;
+	}
+
+	public String getScoreName() {
+		return name;
+	}
+
+	public void setScoreName(String name) {
+		this.name = name;
+	}
+	
+}
+	
